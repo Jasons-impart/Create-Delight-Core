8239c97460327ca8e7e6d7baf38cdeda20d5aea1
CPearl0 <1269596929@qq.com>
2024-11-27 19:24:10 +0800

    Chocoloate fan freezing recipe

--- src/main/java/io/github/jasonsimpart/createdelightcore/data/recipe/FanFreezingRecipeGen.java
+++ src/main/java/io/github/jasonsimpart/createdelightcore/data/recipe/FanFreezingRecipeGen.java
@@ -1,9 +1,11 @@
 package io.github.jasonsimpart.createdelightcore.data.recipe;
 
 import com.simibubi.create.AllItems;
+import io.github.jasonsimpart.createdelightcore.registry.CDItems;
 import io.github.jasonsimpart.createdelightcore.registry.CDRecipeTypes;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.item.Items;
+import org.forsteri.ratatouille.entry.CRItems;
 
 public class FanFreezingRecipeGen extends CDProcessingRecipeGen {
     GeneratedRecipe
@@ -14,7 +16,11 @@ public class FanFreezingRecipeGen extends CDProcessingRecipeGen {
             SLIME_BALL = convert(Items.MAGMA_CREAM, Items.SLIME_BALL),
             SNOW = convert(Items.SNOWBALL, Items.SNOW),
             SNOW_BLOCK = convert(Items.SNOW, Items.SNOW_BLOCK),
-            OBSIDIAN = convert(Items.CRYING_OBSIDIAN, Items.OBSIDIAN);
+            OBSIDIAN = convert(Items.CRYING_OBSIDIAN, Items.OBSIDIAN),
+            CHOCOLATE = convert(CRItems.CHOCOLATE_MOLD_FILLED.get(), CRItems.CHOCOLATE_MOLD_SOLID.get()),
+            BLACK_CHOCOLATE = convert(CDItems.BLACK_CHOCOLATE_MOLD_FILLED.get(), CDItems.BLACK_CHOCOLATE_MOLD_SOLID.get()),
+            WHITE_CHOCOLATE = convert(CDItems.WHITE_CHOCOLATE_MOLD_FILLED.get(), CDItems.WHITE_CHOCOLATE_MOLD_SOLID.get()),
+            RUBY_CHOCOLATE = convert(CDItems.RUBY_CHOCOLATE_MOLD_FILLED.get(), CDItems.RUBY_CHOCOLATE_MOLD_SOLID.get());
 
     public FanFreezingRecipeGen(PackOutput output) {
         super(output);
