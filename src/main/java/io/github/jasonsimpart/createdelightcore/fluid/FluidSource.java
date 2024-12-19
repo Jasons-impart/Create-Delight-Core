47da746f44e91d892e44d1e7ccaa8bb73d97b74f
lumine <463305152@qq.com>
2024-12-19 20:24:24 +0800

    liu ti sheng yin

--- src/main/java/io/github/jasonsimpart/createdelightcore/fluid/FluidSource.java
+++ src/main/java/io/github/jasonsimpart/createdelightcore/fluid/FluidSource.java
@@ -2,6 +2,8 @@ package io.github.jasonsimpart.createdelightcore.fluid;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.FluidState;
@@ -16,14 +18,13 @@ public class FluidSource extends ForgeFlowingFluid.Source {
     public void animateTick(Level pLevel, BlockPos pPos, FluidState pState, RandomSource pRandom) {
         super.animateTick(pLevel, pPos, pState, pRandom);
 
-        // 控制粒子生成概率
         if (pRandom.nextFloat() < 0.3F) {
             double x = pPos.getX() + pRandom.nextDouble();
             double y = pPos.getY() + pRandom.nextDouble();
             double z = pPos.getZ() + pRandom.nextDouble();
 
-            // 使用内置的熔岩粒子
             pLevel.addParticle(ParticleTypes.DRIPPING_LAVA, x, y, z, 0, 0, 0);
+            pLevel.playLocalSound(pPos, SoundEvents.ARMOR_STAND_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F, false);
         }
     }
 }
