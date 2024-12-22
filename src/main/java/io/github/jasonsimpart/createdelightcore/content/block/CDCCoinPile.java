ec2e7415340feae1818e553aa3d1b5a0fb88ff5d
JasonQ1123 <nmuqindexiang@163.com>
2024-12-23 01:04:54 +0800

    增加钱币堆

---
+++ src/main/java/io/github/jasonsimpart/createdelightcore/content/block/CDCCoinPile.java
@@ -0,0 +1,9 @@
+package io.github.jasonsimpart.createdelightcore.content.block;
+
+import net.minecraft.world.level.block.SnowLayerBlock;
+
+public class CDCCoinPile extends SnowLayerBlock {
+    public CDCCoinPile(Properties pProperties) {
+        super(pProperties);
+    }
+}
--- src/main/java/io/github/jasonsimpart/createdelightcore/registry/CDBlocks.java
+++ src/main/java/io/github/jasonsimpart/createdelightcore/registry/CDBlocks.java
@@ -1,6 +1,9 @@
 package io.github.jasonsimpart.createdelightcore.registry;
 
+import com.github.alexthe666.iceandfire.block.IafBlockRegistry;
+import com.github.alexthe666.iceandfire.misc.IafSoundRegistry;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import io.github.jasonsimpart.createdelightcore.content.block.CDCCoinPile;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
@@ -8,16 +11,29 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
 
 import static io.github.jasonsimpart.createdelightcore.registry.CDRegistration.REGISTRATE;
 
 public class CDBlocks {
     public static final ResourceKey<CreativeModeTab> MISC_TAB = CDCreativeTabs.MISC.getKey();
+    public static final ResourceKey<CreativeModeTab> COIN_TAB = CDCreativeTabs.COIN.getKey();
     //block
     public static final BlockEntry<GlassBlock> FRAGMENT_OF_BORDER;
-
+    //coin_pile
+    public static final BlockEntry<CDCCoinPile> IRON_COIN_PILE;
+    public static final BlockEntry<CDCCoinPile> COPPER_COIN_PILE;
+    public static final BlockEntry<CDCCoinPile> GOLD_COIN_PILE;
+    public static final BlockEntry<CDCCoinPile> EMERALD_COIN_PILE;
+    public static final BlockEntry<CDCCoinPile> NETHERITE_COIN_PILE;
 
     static {
+        //coin_pile
+        IRON_COIN_PILE = coinPileBlock("iron_coin_pile", Rarity.COMMON);
+        COPPER_COIN_PILE = coinPileBlock("copper_coin_pile", Rarity.UNCOMMON);
+        GOLD_COIN_PILE = coinPileBlock("gold_coin_pile", Rarity.RARE);
+        EMERALD_COIN_PILE = coinPileBlock("emerald_coin_pile", Rarity.RARE);
+        NETHERITE_COIN_PILE = coinPileBlock("netherite_coin_pile", Rarity.EPIC);
         //noinspection removal
         FRAGMENT_OF_BORDER = REGISTRATE.block("fragment_of_border", GlassBlock::new)
                 .item()
@@ -47,6 +63,22 @@ public class CDBlocks {
                         .strength(2.0F)
                         .explosionResistance(resistance)
                         .destroyTime(hardness)
+                )
+                .register();
+    }
+
+    public static BlockEntry<CDCCoinPile> coinPileBlock(String name, Rarity rarity) {
+        return REGISTRATE.block(name, CDCCoinPile::new)
+                .item()
+                .properties(properties -> properties
+                        .fireResistant()
+                        .rarity(rarity))
+                .tab(COIN_TAB)
+                .build()
+                .properties(properties -> properties
+                        .strength(5.0F)
+                        .sound(IafBlockRegistry.SOUND_TYPE_GOLD)
+                        .pushReaction(PushReaction.DESTROY)
                 )
                 .register();
     }
