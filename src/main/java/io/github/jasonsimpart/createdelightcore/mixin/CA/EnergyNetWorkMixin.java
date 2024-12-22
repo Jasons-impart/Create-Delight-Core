38e66fc13e13322a57baf01c223dae68ad490b73
JasonQ1123 <nmuqindexiang@163.com>
2024-12-22 22:59:44 +0800

    未知原因导致的mixin失败

--- src/main/java/io/github/jasonsimpart/createdelightcore/mixin/CA/EnergyNetWorkMixin.java
+++ src/main/java/io/github/jasonsimpart/createdelightcore/mixin/CA/EnergyNetWorkMixin.java
@@ -2,8 +2,13 @@ package io.github.jasonsimpart.createdelightcore.mixin.CA;
 
 import com.mrh0.createaddition.energy.network.EnergyNetwork;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EnergyNetwork.class)
 public abstract class EnergyNetWorkMixin {
-
+//    @ModifyArg(method = "getMaxBuff", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I"), index = 1)
+//    private int getMaxBuff(int maxBuff) {
+//        return (int) (maxBuff * 2.5F);
+//    }
 }
