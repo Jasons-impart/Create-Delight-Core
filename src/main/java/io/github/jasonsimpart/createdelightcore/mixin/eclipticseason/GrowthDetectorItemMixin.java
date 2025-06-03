14596634ee90565a3602ac1bdef5b50f5a8f5917
SSWTLZZ69 <1824914068@qq.com>
2025-06-03 15:44:45 +0800

    使品质铁及以上的作物不受季节影响，金及以上的作物不受湿度影响

---
+++ src/main/java/io/github/jasonsimpart/createdelightcore/mixin/eclipticseason/GrowthDetectorItemMixin.java
@@ -0,0 +1,35 @@
+package io.github.jasonsimpart.createdelightcore.mixin.eclipticseason;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.teamtea.eclipticseasons.api.constant.biome.Humidity;
+import com.teamtea.eclipticseasons.api.constant.solar.Season;
+import com.teamtea.eclipticseasons.api.data.crop.CropGrowControl;
+import com.teamtea.eclipticseasons.api.data.crop.GrowParameter;
+import com.teamtea.eclipticseasons.common.core.crop.CropGrowthHandler;
+import com.teamtea.eclipticseasons.common.item.GrowthDetectorItem;
+import de.cadentem.quality_food.capability.LevelData;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = GrowthDetectorItem.class, remap = false)
+public class GrowthDetectorItemMixin {
+    @Redirect(method = "getGrowChance", at = @At(value = "INVOKE", target = "Lcom/teamtea/eclipticseasons/api/data/crop/GrowParameter;grow_chance()F"))
+    private static float getGrowChanceMixin(GrowParameter instance, @Local(argsOnly = true) Level level, @Local(argsOnly = true) BlockPos pos) {
+        if (LevelData.get(level, pos).level() > 0)
+            return 1;
+        return instance.grow_chance();
+    }
+
+    @Inject(method = "getHumidityGrowChance", at = @At(value = "HEAD"), cancellable = true)
+    private static void getHumidityGrowChanceMixin(LevelAccessor world, CropGrowControl growControl, Humidity env, CropGrowthHandler.RoomStatus roomStatus, BlockPos pos, BlockState blockState, Season season, boolean hasUpdate, CallbackInfoReturnable<Float> cir) {
+        if (LevelData.get(world, pos).level() > 1)
+            cir.setReturnValue(1.f);
+    }
+}
