fb457bb7d99f6a4bb4903551d572f1bf2d6e3f8b
SSWTLZZ69 <1824914068@qq.com>
2025-07-14 12:41:20 +0800

    修复ae在原材料仅有一份时不会有品质的问题

--- src/main/java/io/github/jasonsimpart/createdelightcore/mixin/ae2/CraftingTermSlotMixin.java
+++ src/main/java/io/github/jasonsimpart/createdelightcore/mixin/ae2/CraftingTermSlotMixin.java
@@ -5,14 +5,19 @@ import appeng.api.stacks.KeyCounter;
 import appeng.api.storage.MEStorage;
 import appeng.menu.slot.CraftingTermSlot;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import de.cadentem.quality_food.util.QualityUtils;
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.TransientCraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -27,20 +32,26 @@ public abstract class CraftingTermSlotMixin {
     abstract InternalInventory getPattern();
     @Shadow(remap = false)
     abstract protected Recipe<CraftingContainer> findRecipe(CraftingContainer ic, Level level);
-    @Final
-    @Shadow(remap = false)
-    private  InternalInventory craftInv;
 
+    @Inject(method = "craftItem", at = @At("HEAD"), remap = false, require = 1)
+    public void storeContainer(Player p, MEStorage inv, KeyCounter all, CallbackInfoReturnable<ItemStack> cir, @Share("itemHandler") LocalRef<IItemHandler> itemHandlerLocalRef) {
+        ItemStackHandler itemStackHandler = new ItemStackHandler(9);
+        for (int i = 0; i < getPattern().size(); i++) {
+            itemStackHandler.setStackInSlot(i, getPattern().getStackInSlot(i));
+        }
+        itemHandlerLocalRef.set(itemStackHandler);
+    }
     @Inject(method = "craftItem", at = @At(value = "RETURN", ordinal = 3), remap = false, require = 1, cancellable = true)
-    public void craftItemMixin(Player p, MEStorage inv, KeyCounter all, CallbackInfoReturnable<ItemStack> cir, @Local(ordinal = 0) ItemStack is) {
+    public void craftItemMixin(Player p, MEStorage inv, KeyCounter all, CallbackInfoReturnable<ItemStack> cir, @Local(ordinal = 0) ItemStack is, @Share("itemHandler") LocalRef<IItemHandler> itemHandlerLocalRef) {
         ItemStack itemStack = is.copy();
         TransientCraftingContainer ic = new TransientCraftingContainer(p.containerMenu, 3, 3);
 
         for(int x = 0; x < 9; ++x) {
-            ic.setItem(x, this.getPattern().getStackInSlot(x));
+            ic.setItem(x, itemHandlerLocalRef.get().getStackInSlot(x));
         }
         Recipe<CraftingContainer> r = this.findRecipe(ic, p.level());
         QualityUtils.handleConversion(itemStack, ic, r, p.level().registryAccess());
         cir.setReturnValue(itemStack);
     }
+
 }
