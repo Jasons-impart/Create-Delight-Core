0bf4d7301d9a901d378b348d93c409200ff32ab1
JIA <430731352@qq.com>
2026-05-23 18:18:25 +0800

    fix: 给Quark赞助者补丁添加开关

--- src/main/java/io/github/jasonsimpart/mixin/quark/ContributorRewardHandlerMixin.java
+++ src/main/java/io/github/jasonsimpart/mixin/quark/ContributorRewardHandlerMixin.java
@@ -1,5 +1,6 @@
 package io.github.jasonsimpart.mixin.quark;
 
+import io.github.jasonsimpart.Config;
 import io.github.jasonsimpart.CreateDelightCore;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,17 +13,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class ContributorRewardHandlerMixin {
     @Inject(method = "init()V", at = @At("HEAD"), cancellable = true)
     private static void cdc$disableContributorRewardLoading(CallbackInfo ci) {
+        if (!Config.ENABLE_QUARK_CONTRIBUTOR_REWARD_PATCH.get()) {
+            return;
+        }
+
         CreateDelightCore.LOGGER.debug("CDC disabled Quark contributor reward loading");
         ci.cancel();
     }
 
     @Inject(method = "getTier(Ljava/lang/String;)I", at = @At("HEAD"), cancellable = true)
     private static void cdc$disableContributorTier(String name, CallbackInfoReturnable<Integer> cir) {
+        if (!Config.ENABLE_QUARK_CONTRIBUTOR_REWARD_PATCH.get()) {
+            return;
+        }
+
         cir.setReturnValue(0);
     }
 
     @Inject(method = "getTier(Lnet/minecraft/world/entity/player/Player;)I", at = @At("HEAD"), cancellable = true)
     private static void cdc$disableContributorTier(Player player, CallbackInfoReturnable<Integer> cir) {
+        if (!Config.ENABLE_QUARK_CONTRIBUTOR_REWARD_PATCH.get()) {
+            return;
+        }
+
         cir.setReturnValue(0);
     }
 }
