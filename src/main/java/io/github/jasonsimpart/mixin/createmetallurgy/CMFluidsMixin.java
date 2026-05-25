4c977cb155c3a8c22f014da6d14cd26553b05e1b
KazeShukufuku <13942976035@163.com>
2026-05-25 19:07:41 +0800

    feat: 阻止冶金学自带的熔融金属流体交互逻辑

---
+++ src/main/java/io/github/jasonsimpart/mixin/createmetallurgy/CMFluidsMixin.java
@@ -0,0 +1,15 @@
+package io.github.jasonsimpart.mixin.createmetallurgy;
+
+import fr.lucreeper74.createmetallurgy.registries.CMFluids;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = CMFluids.class, remap = false)
+public class CMFluidsMixin {
+    @Inject(method = "registerFluidInteractions", at = @At("HEAD"), cancellable = true)
+    private static void createdelightcore$disableMoltenWaterInteraction(CallbackInfo ci) {
+        ci.cancel();
+    }
+}
