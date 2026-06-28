811ba50b377b1569a96244a133b0bc9550b3d82b
JIA <430731352@qq.com>
2026-06-28 17:52:09 +0800

    fix: 修复禁用方块客户端区块同步

--- src/main/java/io/github/jasonsimpart/mixin/minecraft/LevelChunkDisabledBlockMixin.java
+++ src/main/java/io/github/jasonsimpart/mixin/minecraft/LevelChunkDisabledBlockMixin.java
@@ -2,16 +2,21 @@ package io.github.jasonsimpart.mixin.minecraft;
 
 import io.github.jasonsimpart.disabled.DisabledContentManager;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.chunk.ChunkAccess;
-import net.minecraft.world.level.chunk.ImposterProtoChunk;
-import net.minecraft.world.level.chunk.ProtoChunk;
+import net.minecraft.world.level.chunk.LevelChunk;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin({ProtoChunk.class, ImposterProtoChunk.class})
-public abstract class ChunkAccessDisabledBlockMixin {
+@Mixin(LevelChunk.class)
+public abstract class LevelChunkDisabledBlockMixin {
+    @Shadow
+    @Final
+    Level level;
+
     @ModifyVariable(
             method = "setBlockState(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Z)Lnet/minecraft/world/level/block/state/BlockState;",
             at = @At("HEAD"),
@@ -19,6 +24,10 @@ public abstract class ChunkAccessDisabledBlockMixin {
             ordinal = 0
     )
     private BlockState createdelightcore$replaceDisabledBlock(BlockState state, BlockPos pos) {
-        return DisabledContentManager.replacementFor(state, (ChunkAccess) (Object) this, pos);
+        if (this.level.isClientSide) {
+            return state;
+        }
+
+        return DisabledContentManager.replacementFor(state, (LevelChunk) (Object) this, pos);
     }
 }
