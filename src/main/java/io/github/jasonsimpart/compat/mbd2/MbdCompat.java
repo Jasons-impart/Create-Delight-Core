efd39f531cd107be843c6e9b681d23b779d2b353
JIA <jiafa@hello-happy.world>
2026-09-20 21:18:22 +0800

    feat(mbd2): 实现带放置保护与材料退款的温室建造器

--- src/main/java/io/github/jasonsimpart/compat/mbd2/MbdCompat.java
+++ src/main/java/io/github/jasonsimpart/compat/mbd2/MbdCompat.java
@@ -33,7 +33,7 @@ public final class MbdCompat {
             "assembly_line", "assembly_import_hatch", "assemble_import_bus",
             "big_centrifuge", "centrifuge_rotor", "steel_import_bus", "steel_export_bus",
             "fission_reactor", "fission_fuel_assembly", "fission_reactor_controller",
-            "contract_executor", "electrolyzer", "mechanical_craft_encoder", "mortar", "quality_destroyer", "small_centrifugation"
+            "contract_executor", "electrolyzer", "greenhouse_builder", "mechanical_craft_encoder", "mortar", "quality_destroyer", "small_centrifugation"
     };
 
     private MbdCompat() {}
@@ -55,6 +55,7 @@ public final class MbdCompat {
         MbdAssembly.register();
         MbdCraftEncoder.register();
         MbdSmallProcessing.register();
+        MbdGreenhouse.register();
         MbdCentrifuge.register();
         MbdReactor.register();
         NeoForge.EVENT_BUS.addListener(MbdHydropower::formed);
